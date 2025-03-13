10fdf76e255f09c8c28cc32c73fa95ed6affdaff
simonfroggatt <simon@safetysignsandnotices.co.uk>
2025-03-13 18:16:11 +0400

    added  https://js.stripe.com/v3/

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -13,10 +13,14 @@ function getURLVar(key) {
 				value[data[0]] = data[1];
 			}
 		}
-
+		console.log(value);
 		if (value[key]) {
+			console.log('getURLVar');
+			console.log(value);
+			console.log(value[key]);
 			return value[key];
 		} else {
+			console.log('return empty string');
 			return '';
 		}
 	}
